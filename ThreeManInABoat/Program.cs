a17f7facdb7a5bc1ed45922e2964f402d7ba2723
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2021-04-21 16:00:30 +0200

    Možnost vytvoření seznamu všech slov

--- ThreeManInABoat/Program.cs
+++ ThreeManInABoat/Program.cs
@@ -9,6 +9,8 @@ namespace ThreeManInABoat
 {
    public static class FileMoves
     {
+     
+
         public static void ReadAll(string path) 
         {
             string textOut = "";
@@ -18,31 +20,83 @@ namespace ThreeManInABoat
             Console.Write(textOut);
         }
 
-        public static void FindWord(string word)
+        public static void FindWord(string word, string path)
         {
             string lineNums = "";
             string currentLine;
             int lineNum = 1;
-            StreamReader sr = new StreamReader("BaseText.txt");
-            StreamWriter sw = new StreamWriter("WordLines.txt");
+            StreamReader sr = new StreamReader(path);
 
             while (!sr.EndOfStream)
-            {
-                currentLine = sr.ReadLine();
-                if (currentLine.Contains(word))
                 {
-                    lineNums += lineNum + ", ";
-                    lineNum++;
+                    currentLine = sr.ReadLine();
+                    if (currentLine.Contains(word))
+                    {
+                        lineNums += lineNum + ", ";
+                        lineNum++;
+                    }
+                    else
+                    {
+                        lineNum++;
+                    }
                 }
-                else
+            using (StreamWriter sw = new StreamWriter("WordLines.txt", true))
+            {
+                sw.Write(word + " > " + lineNums);
+                sw.WriteLine();
+            }
+
+        }
+
+        public static void WordList (string path)
+        {
+            string textInString;
+            List<string> Unwanted = new List<string>();
+            Unwanted.Add("and");
+            Unwanted.Add("nor");
+            Unwanted.Add("but");
+            Unwanted.Add("or");
+            Unwanted.Add("yet");
+            Unwanted.Add("so");
+            Unwanted.Add("both");
+            Unwanted.Add("either");
+            Unwanted.Add("neither");
+            Unwanted.Add("only");
+            Unwanted.Add("also");
+            Unwanted.Add("in");
+            Unwanted.Add("at");
+            Unwanted.Add("on");
+            Unwanted.Add("by");
+            Unwanted.Add("beside");
+            Unwanted.Add("before");
+            Unwanted.Add("below");
+            Unwanted.Add("under");
+            Unwanted.Add("over");
+            Unwanted.Add("above");
+            Unwanted.Add("across");
+            Unwanted.Add("between");
+            Unwanted.Add("towards");
+            Unwanted.Add("for");
+
+
+            HashSet<string> stringHash = new HashSet<string>();
+            StreamReader sr = new StreamReader(path);
+            textInString = sr.ReadToEnd();
+            
+
+            foreach (string word in textInString.Split(' '))
+            {
+                if (!stringHash.Contains(word) && !Unwanted.Contains(word))
                 {
-                    lineNum++;
+                    stringHash.Add(word);
                 }
             }
-            sr.Close();
-
-            sw.Write(word + " > " + lineNums);
+            foreach (string word in stringHash)
+            {
+                FindWord(word,path);
+            }
         }
+
     }
     class Program
     {
@@ -50,8 +104,10 @@ namespace ThreeManInABoat
         {
           string myPath = "BaseText.txt";
             //FileMoves.ReadAll(myPath);
-            Console.WriteLine("Write your word > ");
-            FileMoves.FindWord(Console.ReadLine());
+            //Console.WriteLine("Write your word > ");
+            //FileMoves.FindWord(Console.ReadLine());
+            FileMoves.WordList(myPath);
+            Console.WriteLine("DONE");
             Console.ReadKey();
         }
 
